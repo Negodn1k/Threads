2c0ac845937d615e4e69f7177df9f66411e06540
Negodn1k <havkaset@gmail.com>
2023-03-16 13:59:05 +0300

    Code added

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.lang.Thread;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Loader loader = new Loader();
+        Breakpointer breakpointer = new Breakpointer();
+
+        Thread loaderThread = new Thread(loader.load);
+        Thread breakThread = new Thread(breakpointer.breaker);
+
+        loaderThread.start();
+        breakThread.start();
+        loaderThread.join();
+    }
+}
+
